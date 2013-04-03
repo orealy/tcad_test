976c2cabe22afb5212bf7482de5118fe45635225
Chris Webb <orealy@gmail.com>
2013-04-03 12:18:17 +1100

    added structure file.

---
+++ structure.scm
@@ -0,0 +1,5 @@
+(ise:clear)
+
+(isegeo:set-default-boolean "XX")
+(isegeo:create-rectangle (position 40 20 0) (position 50 30 0) "Silicon" "region_11")
+(isegeo:create-rectangle (position 43 28 0) (position 47 33 0) "PolySilicon" "region_12")
